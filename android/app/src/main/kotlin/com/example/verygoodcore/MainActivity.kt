10d35dbc5ada182112d5722ab1bd5312b5fc97eb
Yan Rodriguez <yanrodriguez@Yans-MBP.attlocal.net>
2023-11-14 10:02:33 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.flutter_firebase_emulators_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_firebase_emulators_demo/app/app.dart';
+import 'package:flutter_firebase_emulators_demo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
